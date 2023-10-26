7d1cdb01a02d9a88758929592640688498cef589
110360148 <t110360148@ntut.edu.tw>
2023-10-26 21:59:00 +0800

    second

--- app/src/main/java/com/example/lab3_hw1/MainActivity.kt
+++ app/src/main/java/com/example/lab3_hw1/MainActivity.kt
@@ -10,11 +10,23 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    /**/
+    private lateinit var ed_name: EditText
+    private lateinit var tv_text: TextView
+    private lateinit var tv_name: TextView
+    private lateinit var tv_winner: TextView
+    private lateinit var tv_mmora: TextView
+    private lateinit var tv_cmora: TextView
+    private lateinit var btn_scissor: RadioButton
+    private lateinit var btn_stone: RadioButton
+    private lateinit var btn_paper: RadioButton
+    private lateinit var btn_mora: Button
+    /**/
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*
         val ed_name = findViewById<EditText>(R.id.ed_name)
         val tv_text = findViewById<TextView>(R.id.tv_text)
         val tv_name = findViewById<TextView>(R.id.tv_name)
@@ -25,12 +37,23 @@ class MainActivity : AppCompatActivity() {
         val btn_stone = findViewById<RadioButton>(R.id.btn_stone)
         val btn_paper = findViewById<RadioButton>(R.id.btn_paper)
         val btn_mora = findViewById<Button>(R.id.btn_mora)
-
+        */
+        ed_name = findViewById(R.id.ed_name)
+        tv_text = findViewById(R.id.tv_text)
+        tv_name = findViewById(R.id.tv_name)
+        tv_winner = findViewById(R.id.tv_winner)
+        tv_mmora = findViewById(R.id.tv_mmora)
+        tv_cmora = findViewById(R.id.tv_cmora)
+        btn_scissor = findViewById(R.id.btn_scissor)
+        btn_stone = findViewById(R.id.btn_stone)
+        btn_paper = findViewById(R.id.btn_paper)
+        btn_mora = findViewById(R.id.btn_mora)
+        /**/
         btn_mora.setOnClickListener { view: View? ->
             if (ed_name.length() < 1) {
                 tv_text.text = "請輸入玩家姓名"
             } else {
-                tv_name.text = String.format("名字\n%s", ed_name.text.toString())
+                tv_name.text = "名字\n${ed_name.text}"
                 if (btn_scissor.isChecked) {
                     tv_mmora.text = "我方出拳\n剪刀"
                 } else if (btn_stone.isChecked) {
@@ -50,10 +73,7 @@ class MainActivity : AppCompatActivity() {
                     btn_stone.isChecked && computer_random == 0 ||
                     btn_paper.isChecked && computer_random == 1
                 ) {
-                    tv_winner.text = """
-                        勝利者
-                        ${ed_name.text}
-                        """.trimIndent()
+                    tv_winner.text = "勝利者\n${ed_name.text}"
                     tv_text.text = "恭喜您獲勝了! ! !"
                 } else if (btn_scissor.isChecked && computer_random == 1 ||
                     btn_stone.isChecked && computer_random == 2 ||
